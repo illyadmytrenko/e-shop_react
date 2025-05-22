db873d4144d1ab8725b7e3a9d8a8aaa50e8733ce
illyadmytrenko <illadmitrenko666@gmail.com>
2025-05-22 13:03:54 +0300

    ch

--- db.js
+++ db.js
@@ -2,11 +2,11 @@
 import mysql from "mysql2/promise";
 
 const pool = mysql.createPool({
-  host: process.env.VITE_MYSQL_HOST,
-  port: Number(process.env.VITE_MYSQL_PORT),
-  user: process.env.VITE_MYSQL_USER,
-  password: process.env.VITE_MYSQL_PASSWORD,
-  database: process.env.VITE_MYSQL_DATABASE,
+  host: process.env.VITE_MYSQLHOST,
+  port: Number(process.env.VITE_MYSQLPORT),
+  user: process.env.VITE_MYSQLUSER,
+  password: process.env.VITE_MYSQLPASSWORD,
+  database: process.env.VITE_MYSQLDATABASE,
 });
 
 export default pool;
