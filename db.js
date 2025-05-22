1d5034a6b4250a112063e4b93b4105335d1068e3
illyadmytrenko <illadmitrenko666@gmail.com>
2025-05-22 12:59:15 +0300

    env changes again

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool(process.env.VITE_MYSQL_URL);
+const pool = mysql.createPool({
+  host: process.env.VITE_MYSQL_HOST,
+  port: Number(process.env.VITE_MYSQL_PORT),
+  user: process.env.VITE_MYSQL_USER,
+  password: process.env.VITE_MYSQL_PASSWORD,
+  database: process.env.VITE_MYSQL_DATABASE,
+});
 
 export default pool;
