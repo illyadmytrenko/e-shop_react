8bfb1030b03726309fffc1d3265389543fdff1bc
illyadmytrenko <illadmitrenko666@gmail.com>
2025-05-22 12:54:04 +0300

    env

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 import mysql from "mysql2/promise";
 
-const pool = mysql.createPool(process.env.MYSQL_URL);
+const pool = mysql.createPool(process.env.VITE_MYSQL_URL);
 
 export default pool;
