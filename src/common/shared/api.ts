839d7c575f828744126a621a5b283f47405a73be
illyadmytrenko <illadmitrenko666@gmail.com>
2025-05-22 14:31:06 +0300

    finally!!!

--- src/common/shared/api.ts
+++ src/common/shared/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = "https://e-shopreact-production.up.railway.app";
+const baseUrl = "https://e-shopreact-production-3eb1.up.railway.app/";
 
 export const baseApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
